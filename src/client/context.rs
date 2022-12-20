727fb68101a1a836bc522041ae639aae3aacf69b
Borys Chyliński <borchy97@gmail.com>
2022-12-20 01:14:24 +0100

    Cleanup context code.

--- src/client/context.rs
+++ src/client/context.rs
@@ -19,21 +19,31 @@ use futures::{
     channel::{mpsc, oneshot},
     AsyncRead, AsyncWrite, FutureExt, SinkExt, StreamExt,
 };
-use std::{collections::VecDeque, sync::Arc};
+use std::{
+    collections::VecDeque,
+    sync::Arc,
+    time::{Duration, SystemTime},
+};
+
+struct Session {
+    awaiting_ack: VecDeque<(usize, oneshot::Sender<RxPacket>)>,
+    subscriptions: VecDeque<(usize, mpsc::UnboundedSender<RxPacket>)>,
+    unackowledged: VecDeque<(usize, Bytes)>,
+}
+
+struct Connection {
+    disconnection_timestamp: Option<SystemTime>,
+    session_expiry_interval: Duration,
+}
 
 /// Client context. It is responsible for socket management and direct communication with the broker.
 ///
 pub struct Context<RxStreamT, TxStreamT> {
     rx: RxPacketStream<RxStreamT>,
     tx: TxPacketStream<TxStreamT>,
-
-    awaiting_ack: VecDeque<(usize, oneshot::Sender<RxPacket>)>,
-    subscriptions: VecDeque<(usize, mpsc::UnboundedSender<RxPacket>)>,
-    unackowledged: VecDeque<(usize, Bytes)>,
-
     message_queue: mpsc::UnboundedReceiver<ContextMessage>,
-
-    resend_flag: bool,
+    session: Session,
+    connection: Connection,
 }
 
 impl<RxStreamT, TxStreamT> Context<RxStreamT, TxStreamT>
@@ -58,11 +68,16 @@ where
             Self {
                 rx: RxPacketStream::with_capacity(256, rx),
                 tx: TxPacketStream::from(tx),
-                awaiting_ack: VecDeque::new(),
-                subscriptions: VecDeque::new(),
-                unackowledged: VecDeque::new(),
                 message_queue: receiver,
-                resend_flag: false,
+                session: Session {
+                    awaiting_ack: VecDeque::new(),
+                    subscriptions: VecDeque::new(),
+                    unackowledged: VecDeque::new(),
+                },
+                connection: Connection {
+                    disconnection_timestamp: None,
+                    session_expiry_interval: Duration::from_secs(0),
+                },
             },
             ContextHandle {
                 sender,
@@ -72,7 +87,137 @@ where
         )
     }
 
-    /// Makes [Context] object start processing MQTT traffic, blocking the current task/thread until
+    fn is_reconnect(connection: &Connection) -> bool {
+        connection.disconnection_timestamp.is_some()
+    }
+
+    fn session_expired(connection: &Connection) -> bool {
+        debug_assert!(Self::is_reconnect(connection));
+
+        connection.session_expiry_interval
+            >= connection
+                .disconnection_timestamp
+                .and_then(|timestamp| timestamp.elapsed().ok())
+                .unwrap()
+    }
+
+    async fn handle_message(
+        tx: &mut TxPacketStream<TxStreamT>,
+        session: &mut Session,
+        msg: ContextMessage,
+    ) -> Result<(), MqttError> {
+        match msg {
+            ContextMessage::Disconnect(packet) => {
+                tx.write(packet.freeze().as_ref()).await?;
+                // Graceful disconnection.
+            }
+            ContextMessage::FireAndForget(packet) => {
+                tx.write(packet.freeze().as_ref()).await?;
+            }
+            ContextMessage::AwaitAck(mut msg) => {
+                tx.write(msg.packet.as_ref()).await?;
+                session
+                    .awaiting_ack
+                    .push_back((msg.action_id, msg.response_channel));
+
+                let fixed_hdr = msg.packet.get_mut(0).unwrap();
+                let packet_id = *fixed_hdr >> 4;
+
+                if packet_id == PublishTx::PACKET_ID {
+                    *fixed_hdr |= (1 << 3) as u8; // Set DUP flag in the PUBLISH fixed header
+                    session
+                        .unackowledged
+                        .push_back((msg.action_id, msg.packet.freeze()));
+                } else if packet_id == PubrelTx::PACKET_ID {
+                    session
+                        .unackowledged
+                        .push_back((msg.action_id, msg.packet.freeze()));
+                }
+            }
+            ContextMessage::Subscribe(msg) => {
+                session
+                    .awaiting_ack
+                    .push_back((msg.action_id, msg.response_channel));
+                session
+                    .subscriptions
+                    .push_back((msg.subscription_identifier, msg.stream));
+                tx.write(msg.packet.freeze().as_ref()).await?;
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn handle_packet(session: &mut Session, packet: RxPacket) -> Result<(), MqttError> {
+        match packet {
+            RxPacket::Publish(publish) => match publish.subscription_identifier {
+                Some(subscription_identifier) => {
+                    let sub_id = NonZero::from(subscription_identifier).get().value() as usize;
+                    let maybe_pos = linear_search_by_key(&session.subscriptions, sub_id);
+
+                    if let Some((_, subscription)) =
+                        maybe_pos.map(|pos| &mut session.subscriptions[pos])
+                    {
+                        // User may drop the receiving stream,
+                        // in that case remove it from the active subscriptions map.
+                        if (subscription.send(RxPacket::Publish(publish)).await).is_err() {
+                            linear_search_by_key(&session.subscriptions, sub_id)
+                                .and_then(|pos| session.subscriptions.remove(pos));
+                        }
+                    }
+                }
+                None => {
+                    return Err(
+                        CodecError::MandatoryPropertyMissing(MandatoryPropertyMissing).into(),
+                    )
+                }
+            },
+            RxPacket::Disconnect(disconnect) => {
+                if disconnect.reason == DisconnectReason::Success {
+                    return Ok(()); // Graceful disconnection.
+                }
+
+                return Err(disconnect.into());
+            }
+            other => {
+                let action_id = rx_action_id(&other);
+
+                if let RxPacket::Puback(_) = &other {
+                    linear_search_by_key(&session.unackowledged, action_id)
+                        .and_then(|pos| session.unackowledged.remove(pos));
+                } else if let RxPacket::Pubcomp(_) = &other {
+                    linear_search_by_key(&session.unackowledged, action_id)
+                        .and_then(|pos| session.unackowledged.remove(pos));
+                }
+
+                if let Some((_, sender)) = linear_search_by_key(&session.awaiting_ack, action_id)
+                    .and_then(|pos| session.awaiting_ack.remove(pos))
+                {
+                    // Error here indicates internal error, the receiver
+                    // end is inside one of the ContextHandle methods.
+                    sender.send(other).map_err(|_| HandleClosed)?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn retransmit(
+        tx: &mut TxPacketStream<TxStreamT>,
+        connection: &mut Connection,
+        session: &mut Session,
+    ) -> Result<(), MqttError> {
+        connection.disconnection_timestamp = None;
+
+        for (_, packet) in session.unackowledged.iter() {
+            tx.write(packet.as_ref()).await?;
+        }
+
+        Ok(())
+    }
+
+    /// Makes [Context] object start processing MQTT traffic, blocking (on .await) the current task until
     /// graceful disconnection or error. Successful disconnection via [disconnect] method or
     /// receiving a [Disconnect](https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901205)
     /// packet with reason a code equal to 0 (success) is considered a graceful disconnection.
@@ -82,106 +227,45 @@ where
         RxStreamT: AsyncRead + Unpin,
         TxStreamT: AsyncWrite + Unpin,
     {
-        let mut pck_fut = self.rx.next().fuse();
-        let mut msg_fut = self.message_queue.next().fuse();
+        let tx = &mut self.tx;
+        let rx = &mut self.rx;
+        let message_queue = &mut self.message_queue;
+        let session = &mut self.session;
+        let connection = &mut self.connection;
 
-        // Resend all unacknowledged packets
-        if self.resend_flag {
-            for (_, packet) in self.unackowledged.iter() {
-                self.tx.write(packet.as_ref()).await?;
-                self.resend_flag = false;
+        if Self::is_reconnect(connection) {
+            if Self::session_expired(connection) {
+                session.awaiting_ack.clear();
+                session.subscriptions.clear();
+                session.unackowledged.clear();
             }
+
+            Self::retransmit(tx, connection, session).await?;
         }
+
+        let mut pck_fut = rx.next().fuse();
+        let mut msg_fut = message_queue.next().fuse();
 
         loop {
             futures::select! {
                 maybe_rx_packet = pck_fut => {
                     match maybe_rx_packet {
-                        Some(rx_packet) => match rx_packet? {
-                            RxPacket::Publish(publish) => match publish.subscription_identifier {
-                                Some(subscription_identifier) => {
-                                    let sub_id = NonZero::from(subscription_identifier).get().value() as usize;
-                                    let maybe_pos = linear_search_by_key(&self.subscriptions, sub_id);
-
-                                    if let Some((_, subscription)) = maybe_pos.map(|pos| &mut self.subscriptions[pos])  {
-                                        // User may drop the receiving stream,
-                                        // in that case remove it from the active subscriptions map.
-                                        if (subscription.send(RxPacket::Publish(publish)).await).is_err() {
-                                            linear_search_by_key(&self.subscriptions, sub_id).and_then(|pos| self.subscriptions.remove(pos));
-                                        }
-                                    }
-                                }
-                                None => {
-                                    return Err(
-                                        CodecError::MandatoryPropertyMissing(MandatoryPropertyMissing).into(),
-                                    )
-                                }
-                            },
-                            RxPacket::Disconnect(disconnect) => {
-                                if disconnect.reason == DisconnectReason::Success {
-                                    return Ok(()); // Graceful disconnection.
-                                }
-
-                                return Err(disconnect.into());
-                            }
-                            other => {
-                                let action_id = rx_action_id(&other);
-
-                                if let RxPacket::Puback(_) = &other {
-                                    linear_search_by_key(&self.unackowledged, action_id).and_then(|pos| self.unackowledged.remove(pos));
-                                } else if let RxPacket::Pubcomp(_) = &other {
-                                    linear_search_by_key(&self.unackowledged, action_id).and_then(|pos| self.unackowledged.remove(pos));
-                                }
-
-                                if let Some((_, sender)) = linear_search_by_key(&self.awaiting_ack, action_id).and_then(|pos| self.awaiting_ack.remove(pos)) {
-                                    // Error here indicates internal error, the receiver
-                                    // end is inside one of the ContextHandle methods.
-                                    sender.send(other).map_err(|_| HandleClosed)?;
-                                }
-                            }
-                        },
+                        Some(rx_packet) => Self::handle_packet(session, rx_packet?).await?,
                         None => {
-                            self.resend_flag = true;
+                            connection.disconnection_timestamp = Some(SystemTime::now());
                             return Err(SocketClosed.into())
                         },
                     }
 
-                    pck_fut = self.rx.next().fuse();
+                    pck_fut = rx.next().fuse();
                 },
                 maybe_msg = msg_fut => {
                     match maybe_msg {
-                        Some(msg) => match msg {
-                            ContextMessage::Disconnect(packet) => {
-                                self.tx.write(packet.freeze().as_ref()).await?;
-                                return Ok(()) // Graceful disconnection.
-                            },
-                            ContextMessage::FireAndForget(packet) => {
-                                self.tx.write(packet.freeze().as_ref()).await?;
-
-                            },
-                            ContextMessage::AwaitAck(mut msg) => {
-                                self.tx.write(msg.packet.as_ref()).await?;
-                                self.awaiting_ack.push_back((msg.action_id, msg.response_channel));
-
-                                let packet_id = msg.packet.get_mut(0).unwrap();
-
-                                if *packet_id == PublishTx::PACKET_ID {
-                                    *packet_id |= (1 << 3) as u8; // Set DUP flag in the PUBLISH fixed header
-                                    self.unackowledged.push_back((msg.action_id, msg.packet.freeze()));
-                                } else if *packet_id == PubrelTx::PACKET_ID {
-                                    self.unackowledged.push_back((msg.action_id, msg.packet.freeze()));
-                                }
-                            },
-                            ContextMessage::Subscribe(msg) => {
-                                self.awaiting_ack.push_back((msg.action_id, msg.response_channel));
-                                self.subscriptions.push_back((msg.subscription_identifier, msg.stream));
-                                self.tx.write(msg.packet.freeze().as_ref()).await?;
-                            },
-                        }
+                        Some(msg) => Self::handle_message(tx, session, msg).await?,
                         None => return Err(HandleClosed.into()),
                     }
 
-                    msg_fut = self.message_queue.next().fuse();
+                    msg_fut = message_queue.next().fuse();
                 }
             }
         }
